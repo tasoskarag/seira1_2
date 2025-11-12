d6f441030da38d31e2a38da7110b5991b1f43198
tasoskarag <anastasios91177@gmail.com>
2025-11-12 17:36:45 +0100

    seira 4

--- ex4_2.cpp
+++ ex4_2.cpp
@@ -1,58 +1,73 @@
 #include "pzhelp"
 
-PROGRAM {
+PROGRAM
+{
 
-int l = READ_INT();
-SKIP_LINE();
+    int l = READ_INT();
+    SKIP_LINE();
 
-char a[100][23];
-int h[100];
-int sum = 0;
-    
-    for (int i=0; i<l; i++) { 
-        // scanf("%21s", a[i]); 
+    char a[100][23];
+    int h[100];
+    int sum = 0;
+
+    for (int i = 0; i < l; i++)
+    {
+        // scanf("%21s", a[i]);
         fgets(a[i], 23, stdin);
         // SKIP_LINE();
-        
+
         int len = strlen(a[i]);
 
-        if(len>21) {
-            h[i]=-1;
-            a[i][21]='\0';
-            fseek(stdin,0,SEEK_END);
-        }  
-        else 
-            a[i][len-1] = '\0'; 
+        if (len > 21)
+        {
+            h[i] = -1;
+            a[i][21] = '\0';
+            fseek(stdin, 0, SEEK_END);
+        }
+        else
+            a[i][len - 1] = '\0';
     }
 
-        for (int i=0; i<l; i++) {
-            int length = strlen(a[i]);
-            int x = length/2;
-        
-            if(length==0) {  
+    for (int i = 0; i < l; i++)
+    {
+        int length = strlen(a[i]);
+        int x = length / 2;
+
+        if (length == 0)
+        {
             WRITELN("empty");
             continue;
-            }
+        }
 
-            if(h[i] == -1) {  
+        if (h[i] == -1)
+        {
             WRITELN("error");
             continue;
-            }
-
-            for (int j=0; j<x; j++) {  
-                if (a[i][j]==a[i][length-1-j])
-                    h[i] = 1; 
-                else h[i]=0;
-            }
-             
-            if (h[i]==1) {
-                sum = sum + 1;
-                WRITELN("yes");
-            }
-            else if (h[i]==0) WRITELN("no");
-            else if (h[i]==-1) WRITELN("error");
         }
-        
-        double g = sum*100/l;
-        if (l>0) WRITELN(FORM(g, 0, 3));
+
+        for (int j = 0; j < x; j++)
+        {
+            if (a[i][j] == a[i][length - 1 - j])
+                h[i] = 1;
+            else
+            {
+                h[i] = 0;
+                break;
+            }
+        }
+
+        if (h[i] == 1)
+        {
+            sum = sum + 1;
+            WRITELN("yes");
+        }
+        else if (h[i] == 0)
+            WRITELN("no");
+        else if (h[i] == -1)
+            WRITELN("error");
+    }
+
+    double g = sum * 100 / l;
+    if (l > 0)
+        WRITELN(FORM(g, 0, 3));
 }
